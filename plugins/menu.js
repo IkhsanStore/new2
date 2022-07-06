b49744ed074dba5f8bf6c0f215a38f44faa0a101
IKHSAN STORE <91927659+IkhsanStore@users.noreply.github.com>
2022-07-06 17:46:54 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -158,10 +158,10 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     let url = `https://i.postimg.cc/nrMn3sMN/menu.jpg`
-    conn.sendHydrated(m.chat, text.trim(), 'SANS STORE', null, 'https://github.com/IkhsanStore', 'MY GITHUB', '', '', [
-      ['𝗚𝗥𝗨𝗣 𝗕𝗢𝗧', '/donasi'],
-      ['?𝗝𝗔𝗦𝗔 𝗥𝗨𝗡?', '/sewa'],
-      ['!𝗢𝗪𝗡𝗘𝗥!', '/owner']
+    conn.sendHydrated(m.chat, text.trim(), 'SANS STORE', null, 'https://instagram.com/ikhsan_sstore', 'INSTAGRAM', '', '', [
+      ['JB SANS STORE', '/donasi'],
+      ['SEWA', '/sewa'],
+      ['!OWNER!', '/owner']
     ], m)
     /*let url = `https://telegra.ph/file/ab1df70dfd5c2bac64da1.jpg`.trim()
     let res = await fetch(url)
